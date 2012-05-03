cf4fe2ca302c373be90dd25c243965d9d608fced
szymon.kapeniak@gmail.com <szymon.kapeniak@gmail.com@aa184569-114b-6ed7-d87b-ef36bec66f58>
2012-05-03 10:03:27 +0000

    Working prototype for rat deep reader. Only RGBA atm. Changed the name from ratDeepReader to ratReaderDeep as it seems to be the one picked up by Nuke (where as dtex examples implies the former name).
    git-svn-id: http://rat4nuke.googlecode.com/svn/trunk@12 aa184569-114b-6ed7-d87b-ef36bec66f58

--- src/ratReaderDeep.cpp
+++ src/ratReaderDeep.cpp
@@ -62,6 +62,7 @@ public:
 
 class ratDeepReader : public DeepReader
 {
+    static Lock sLibraryLock;
     IMG_DeepShadow *rat;
     Lock lock;
     int  xres, yres, nchannels;
@@ -76,11 +77,15 @@ ratDeepReader(DeepReaderOwner* op, const std::string& filename) : DeepReader(op)
     outputContext = _owner->readerOutputContext();
 
     rat = new IMG_DeepShadow();
-    if(!rat->open(filename.buffer());
+    if(!rat->open(filename.c_str()))
         return;
 
+    #if defined(DEBUG)
+    _op->warning("rat opened");
+    #endif
+
     rat->resolution(xres, yres);
-    rat->getChannelCount(nchannels);
+    nchannels = rat->getChannelCount();
 
     setInfo(xres, yres, outputContext, Mask_RGB | Mask_Alpha | Mask_Deep);
 
@@ -88,12 +93,16 @@ ratDeepReader(DeepReaderOwner* op, const std::string& filename) : DeepReader(op)
     //_metaData.setData("dtex/nl", Nl, 16);
 }
 
-~dtexDeepReader()
+~ratDeepReader()
 {
     if (rat) 
     {
         rat->close();
         rat = NULL;
+            
+        #if defined(DEBUG)
+        _op->warning("rat deleted");
+        #endif
     }
 }
 
@@ -103,106 +112,98 @@ open(const std::string& filename){}
 void 
 doDeepEngine(DD::Image::Box box, const ChannelSet& channels, DeepOutputPlane& plane)
 {
-    Guard g(_engineLock);
+    Guard g(lock);
     if (!rat)
         _op->error("error opening file");
  
-    const IMG_DeepShadowChannel *chp;
-    IMG_DeepPixelReader          pixel(&rat);
+    const IMG_DeepShadowChannel *cp;
+    const IMG_DeepShadowChannel *zp;
+    IMG_DeepPixelReader pixel(*rat);
 
-    //DtexImage* image;
-    //DtexGetImageByIndex(_dtexFile, 0, &image); 
-    
+     #if defined(DEBUG)
+      _op->warning("Pixel Reader created.");
+     #endif
+
     int height = yres;
-    //DtexPixel* pixel = DtexMakePixel(_numChans);
     plane = DeepOutputPlane(channels, box);
 
     // TODO: RGBA for now only:
     std::map<Channel, int> channel_map;
     if (nchannels) // if (nchannels == 4)
     {
-        chanMap[Chan_Red] = 0;
-        chanMap[Chan_Green] = 1;
-        chanMap[Chan_Blue] = 2;
-        chanMap[Chan_Alpha] = 3;
-        raw = true;
+        channel_map[Chan_Red] = 0;
+        channel_map[Chan_Green] = 1;
+        channel_map[Chan_Blue] = 2;
+        channel_map[Chan_Alpha] = 3;
+        //channel_map[Chan_Z] = 4;
+        //raw = true;
     } 
     else 
         _op->error("Can't find any channels");// chanMap[Chan_Alpha] = 0;
 
-    std::vector<float> pts (nchannels);
-    std::vector<float> pts2(nchannels);
+    zp = NULL; 
+    cp = NULL;
+
+    for (int i = 0; i < nchannels; i++)
+    {
+        if (strcmp(rat->getChannel(i)->getName(), "C") == 0)
+            cp = rat->getChannel(i);
+        else if (strcmp(rat->getChannel(i)->getName(), "Pz") == 0)
+            zp = rat->getChannel(i);
+
+        #if defined(DEBUG)
+        if (cp && zp)
+            _op->warning("Found channels: %s, %s", cp->getName(), zp->getName());
+        #endif
+    }
+
+    #if defined(DEBUG)
+    _op->warning("channels.size(): %i", channels.size());
+    printf("Channels: ");
+    foreach(chan, channels)   
+        printf("%s, ", getName(chan));
+    printf("\n");
+    #endif
+
+     std::vector<float> pts (4); // FIXME: nchannels
+     //std::vector<float> pts2(nchannels); // FIXME: composited output (not raw).
 
     for (Box::iterator it = box.begin(); it != box.end(); it++)
     {
         float x = it.x;
         float y = it.y;
-        _outputContext.from_proxy_xy(x, y);
+        outputContext.from_proxy_xy(x, y);
         
-        pixel->open(x, height - 1 - y);
-        int numpts = pixel->getDepth();
+        pixel.open(x, height - 1 - y);
+        int numpts = pixel.getDepth();
 
-        DeepOutPixel pels(numpts * channels.size());
+        DeepOutPixel pels(numpts * 6);// FIXME: channels.size()  
 
-    }
+        const float *color;
+        const float *zdepth;
+        for (int i = 0; i < numpts; i++)
+        {
+            color = pixel.getData(*cp, i);
+            zdepth = pixel.getData(*zp, i);
 
-      /*
-      DtexGetPixel(image, int(x), height - 1 - int(y), pixel);
-
-      int numpts = DtexPixelGetNumPoints(pixel);
-
-      DeepOutPixel pels(numpts * channels.size());
-
-      for (int j = 0; j < numpts; j ++ ) {
-          
-        float z;
-        DtexPixelGetPoint(pixel, j, &z, &pts[0]);
-        
-        if (!raw) {
-          float z2;
-          DtexPixelGetPoint(pixel, j+1, &z2, &pts2[0]);
-
-          if (pts[chanMap[Chan_Alpha]] == pts2[chanMap[Chan_Alpha]])
-            continue;
-        }
-        
-        float alpha = 1;
-
-        if (chanMap.count(Chan_Alpha)) {
-          const int alphaChan = chanMap[Chan_Alpha];
-          
-          alpha = raw ? 
-            pts[alphaChan] :
-            ((pts[alphaChan] - pts2[alphaChan]) / pts[alphaChan]);
-        }
-
-        foreach(chan, channels) {
-
-          if (chan == Chan_Z)
-            pels.push_back(1/z);
-          else if (chan == Chan_DeepFront || chan == Chan_DeepBack)
-            pels.push_back(z);
-          else if (chan == Chan_Alpha)
-            pels.push_back(alpha);
-          else if (chanMap.count(chan)) {
-            if (premult && IsRGB(chan) && chanMap.count(Chan_Alpha)) {
-              pels.push_back(pts[chanMap[chan]] * alpha);
+            foreach(chan, channels)
+            {
+                if (chan == Chan_Z)
+                    pels.push_back(1/zdepth[0]);
+                else if (chan == Chan_DeepFront )
+                    pels.push_back(zdepth[0]);
+                else if ( chan == Chan_DeepBack )
+                    pels.push_back(zdepth[0]);
+                else if (chan == Chan_Alpha)
+                    pels.push_back(color[3]);
+                else if (channel_map.count(chan))
+                    pels.push_back( color[channel_map[chan]]);
             }
-            else
-              pels.push_back(pts[chanMap[chan]]);              
-          }
-          else if (IsRGBA(chan))
-            pels.push_back(alpha);
-          else
-            pels.push_back(0);
         }
-      }
-
-      plane.addPixel(pels);
+        plane.addPixel(pels);
     }
-    
-    DtexDestroyPixel(pixel);
-  }*/
+    pixel.close();
+}
 };
 
 static DeepReader* build(DeepReaderOwner* op, const std::string& fn)
@@ -217,6 +218,6 @@ static DeepReaderFormat* buildFormat(DeepReaderOwner* op)
 
 static const DeepReader::Description d("rat\0dcm\0dsm\0", "rat", build, buildFormat);
 
-
-  }
-}
+Lock ratDeepReader::sLibraryLock;
+    } // End of namespace Deep
+} // End of namespace Nuke
